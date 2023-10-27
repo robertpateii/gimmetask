552d6e3b4301d32e6ad9ac5beb3876846c09b31c
Robert Pate <robert.pate@gmail.com>
2023-10-27 01:36:37 +0000

    Add notes for next time

--- gimme.c
+++ gimme.c
@@ -25,8 +25,8 @@ void addTasks() {
 	strcpy(tasks[1], "test task #2\n");
 
 	char tempTask[81];
-	printf("Enter a tasks, 80 char max, type done to stop\n");
-	int i = 2;
+	printf("Enter tasks, 80 char max, type done to stop\n");
+	int i = 2; // go back to 0 once test tasks are removed above
 	char* doneStr = "done\n";
 	while (fgets (tempTask, 81, stdin) != NULL) {
 		if (strcmp(tempTask, doneStr)) { // 0 is match
@@ -47,6 +47,14 @@ void addTasks() {
 
 	}
 
+}
+
+void addMore() {
+	// find first empty string in array (if none, bail)
+	// get new task
+	// add to the empty slot
+	// send back to nextAction()?
+	// could allow for adding many at once here, but would that be needed much? would need to say done everytime you add one or something instead of just a blah blah enter
 }
 
 int countTasks() {
@@ -100,15 +108,29 @@ int main() {
 		switch (nextAction) {
 		case 1:
 			printf("Got Complete\n");
+			// no real different than delete as long as everything is in ram
+			// exclude task from being randomly selected, or shown in lists if that is added
+			// so strpy a blank string on it?
 			break;
 		case 2:
 			printf("Got Skip\n");
+			// should prob be its own function since doing it at least twice 
+			// also it gets a random number within the task count
+			// but then uses it as the position. If I start adding/removing it
+			// will get way off.
+			currTaskPos = getTask(length);
+			printf("Do #%d next:\n", currTaskPos+1);
+			printf(tasks[currTaskPos]);
 			break;
 		case 3:
 			printf("Got Delete\n");
+			// no real different than complete  as long as everything is in ram
+			// exclude task from being randomly selected, or shown in lists if that is added
+			// so strpy a blank string on it?
 			break;
 		case 4:
 			printf("Got Add\n");
+			addMore();
 			break;
 		default:
 			printf("Got unexpected\n");
